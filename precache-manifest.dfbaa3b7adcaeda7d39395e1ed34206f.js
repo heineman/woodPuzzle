c1055b42517020c4420dda6429d8eda3783d16cc
George Heineman <heineman@wpi.edu>
2022-06-10 09:45:18 -0400

    Updates

---
+++ precache-manifest.dfbaa3b7adcaeda7d39395e1ed34206f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "643239a653ef83e1cbad",
+    "url": "/woodPuzzle/static/css/main.dd1c17cc.chunk.css"
+  },
+  {
+    "revision": "643239a653ef83e1cbad",
+    "url": "/woodPuzzle/static/js/main.27bee4fe.chunk.js"
+  },
+  {
+    "revision": "96f1e37801a7bf6d3236",
+    "url": "/woodPuzzle/static/js/runtime~main.99544d09.js"
+  },
+  {
+    "revision": "a5005486ea27944dc76f",
+    "url": "/woodPuzzle/static/js/2.1d3f7e8f.chunk.js"
+  },
+  {
+    "revision": "28d67fdb2f2552e0b292d243b4d7f26f",
+    "url": "/woodPuzzle/index.html"
+  }
+];
